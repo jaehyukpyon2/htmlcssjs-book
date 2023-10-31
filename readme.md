a093f13d2d7842140df25b37e57bf5471fb9bb68
jaehyukpyon <ipadforjay@gmail.com>
2023-10-31 11:26:02 +0900

    add readme.md

---
+++ readme.md
@@ -0,0 +1,21 @@
+# 반응형 Portfolio 템플릿
+HTML + CSS + 자바스크립트 책을 참고하여 화면 사이즈에 따라서 layout이 자동으로 변하는 반응형 포트폴리오 템플릿을 구성
+
+## 목표
+ display 속성 flex와 grid를 사용하여 요소를 배치하고, viewport 사이즈가 변경될 시, @media 쿼리를 사용해서 동적으로 요소들의 크기를 변경한다
+ 자바스크립트를 활용해서 header 요소의 background-color를 변경한다
+
+## 사용 기술
+<img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
+<img src="https://img.shields.io/badge/css3-1572B6?style=for-the-badge&logo=css3&logoColor=white"> 
+<img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=white">
+
+## 배운점
+
+ 1. display flex와 grid를 사용해서 요소를배치하는 방법
+ 2.  @media를 사용해서 viewport 사이즈가 변할 때 요소들의 속성들을 변경하는 방법
+ 3. @keyframes를 사용해서 사용자 정의 animation 효과를 설정하는 방법
+ 4. Javascript를 사용해서  scroll bar의 위치를 구한 후 그 위치에 따라서 동적으로 요소들의 클래스 속성 등을 제어하는 방법 
+
+## Demo
+![htmlcssjsdemo](https://github.com/jaehyukpyon2/htmlcssjs-book/assets/145942491/52a65d87-f01b-4eb2-80d5-1c8efceb2a7d)
--- script.js
+++ script.js
@@ -10,33 +10,37 @@
   let currentTxt = txtArr[index].split("");
   function writeTxt() {
     spanEl.textContent += currentTxt.shift();
+    // currentTxt로부터 앞에서 한 글자씩 빼서 더하기
     if (currentTxt.length !== 0) {
       setTimeout(writeTxt, Math.floor(Math.random() * 100));
     } else {
       currentTxt = spanEl.textContent.split("");
+      // 만약 currentTxt를 다 소비했다면, spanEl 태그의 textContent 영역에 다 누적어 있으니 그걸 split 함수를 통해 배열로 다시 currentTxt에 
+      // 저장
       setTimeout(deleteTxt, 3000);
     }
   }
   function deleteTxt() {
-    currentTxt.pop();
-    spanEl.textContent = currentTxt.join("");
-    if (currentTxt.length !== 0) {
+    currentTxt.pop(); // 가장 우측에서 하나 뺌
+    spanEl.textContent = currentTxt.join(""); // 하나 뺀 상태에서 다시 문자열로 결합
+    if (currentTxt.length !== 0) { // currentTxt의 길이가 아직 0이 아니라면 남아있는 것이니 반복
       setTimeout(deleteTxt, Math.floor(Math.random() * 100));
     } else {
-      index = (index + 1) % txtArr.length;
-      currentTxt = txtArr[index].split("");
-      writeTxt();
+      index = (index + 1) % txtArr.length; // index 값이 넘치지 않게 modulo 연산
+      currentTxt = txtArr[index].split(""); // 다음 text를 잡아다가 split을 사용해서 배열로 return
+      writeTxt(); // 다시 작성하는 거 반복
     }
   }
   writeTxt();
 })();
 
-const headerEl = document.querySelector("header");
+const headerEl = document.querySelector("header"); // header 잡음
 window.addEventListener("scroll", function () {
   requestAnimationFrame(scrollCheck);
 });
 function scrollCheck() {
   let browerScrollY = window.scrollY ? window.scrollY : window.pageYOffset;
+  // 상하 스크롤바의 위치를 구해서 그게 상단에 붙어있지 않으면 active를 추가하고, 만약 상단에 붙어있으면 active를 제거
   if (browerScrollY > 0) {
     headerEl.classList.add("active");
   } else {
